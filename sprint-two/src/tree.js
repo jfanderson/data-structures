2c1304c93e0914ad75603044cb34c4fb2aa0fc78
jfanderson <jfanderson373@gmail.com>
2015-08-19 20:54:27 -0700

    Implemented tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,29 +1,39 @@
-var Tree = function(value){
+var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-  // your code here
-  newTree.children = null;  // fix me
-
-
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
-
+treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
 
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Constructor: O(1)
+ * addChild: O(1)
+ * contains: O(N*M) where N is the number of nodes in the tree rooted by the node contains is called on,
+ * and M is the length of each node's children array
  */
