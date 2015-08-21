4fb47ff580b79375a6360f71171fb0727c19f669
jfanderson <jfanderson373@gmail.com>
2015-08-21 12:01:08 -0700

    Started implementing doublyLinkedList

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,51 @@
+var doublyLinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+  };
+
+  list.removeHead = function(){
+    var value = list.head.value;
+    list.head = list.head.next;
+
+    return value;
+  };
+
+  list.contains = function(target){
+    var temp = list.head;
+    while (temp) {
+      if (temp.value === target) {
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
+  };
+
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ *  addToTail: O(1)
+ *  removeHead: O(1)
+ *  contains: O(N)
+ */
