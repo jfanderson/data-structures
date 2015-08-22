96f046d46c1ce1cb21d2dfaf79768becc2ea207e
jfanderson <jfanderson373@gmail.com>
2015-08-22 11:01:25 -0700

    Implemented doubling and halving of the hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -35,11 +35,23 @@ HashTable.prototype.insert = function(k, v) {
   }
 
   function checkDouble() {
-    console.log(this._total);
     if (this._total + 1 >= Math.round(this._limit * .75)) {
-      this._limit *= 2;
+
+      var newHashTable = new HashTable();
+      newHashTable._limit = this._limit * 2;
+      newHashTable._storage = LimitedArray(newHashTable._limit);
+
+      this._storage.each(function(value) {
+        if (value) {
+          for (var i = 0; i < value.length; i++) {
+            newHashTable.insert(value[i][0], value[i][1]);
+          }
+        }
+      });
+
+      this._limit = newHashTable._limit;
+      this._storage = newHashTable._storage;
     }
-    console.log(this._total);
   }
 };
 
@@ -67,8 +79,29 @@ HashTable.prototype.remove = function(k) {
       if (bucket[j][0] === k) {
         bucket.splice(j, 1);
         this._storage.set(i, bucket);
+        checkHalve.call(this);
         this._total--;
       }
+    }
+  }
+
+  function checkHalve() {
+    if (this._total - 1 <= Math.round(this._limit * .25) && this._limit > 8) {
+
+      var newHashTable = new HashTable();
+      newHashTable._limit = this._limit / 2;
+      newHashTable._storage = LimitedArray(newHashTable._limit);
+
+      this._storage.each(function(value) {
+        if (value) {
+          for (var i = 0; i < value.length; i++) {
+            newHashTable.insert(value[i][0], value[i][1]);
+          }
+        }
+      });
+
+      this._limit = newHashTable._limit;
+      this._storage = newHashTable._storage;
     }
   }
 };
@@ -78,7 +111,7 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  * Constructor: O(1)
- * insert:      O(n)
- * retrieve:    O(1)
+ * insert:      O(n) where n is the length of the hashTable
+ * retrieve:    O(m) where m is the length of the array in the bucket
  * remove:      O(1)
  */
